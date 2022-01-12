2122ddc433d6b336bdbdf559379d8c80b848c331
Anand raja <meetanandns@gmail.com>
2022-01-12 13:04:27 +0000

    dif. ways of comp registration

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,8 @@ import TheHeader from './components/TheHeader.vue';
 export default {
   components: {
     // 'the-header': TheHeader,
-    TheHeader: TheHeader
+    // TheHeader: TheHeader,
+    TheHeader
   },
   data() {
     return {

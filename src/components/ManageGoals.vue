8472ba5abcc44c9816392fbc65bd8f1a574dad8a
Anand raja <meetanandns@gmail.com>
2022-01-12 16:10:35 +0000

    error componnet

--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -1,6 +1,38 @@
 <template>
   <h2> Manage Goals </h2>
   <div>
-    <input type="text">
+    <input type="text" ref="goal">
+    <button @click="setGoal">set goal</button>
   </div>
+  <error-alert v-if="isValueInvalid">
+    <h2>Input is invalid!</h2>
+    <p>Please enter atleast some characters...</p>
+    <button @click="confirmError">Okay</button>
+  </error-alert>
 </template>
+
+<script>
+import ErrorAlert from './ErrorAler.vue';
+
+export default {
+  components: {
+    ErrorAlert
+  },
+  data() {
+    return {
+      isValueInvalid: false
+    };
+  },
+  methods: {
+    setGoal() {
+      const enteredGoal = this.$refs.goal.value;
+      if(enteredGoal.trim() === '') {
+        this.isValueInvalid = true;
+      }
+    },
+    confirmError() {
+      this.isValueInvalid = false;
+    }
+  }
+}
+</script>

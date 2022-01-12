45da4ea752b2a8462c758365197524ab6d3c66ee
Anand raja <meetanandns@gmail.com>
2022-01-12 16:54:26 +0000

    teleport

--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -4,11 +4,14 @@
     <input type="text" ref="goal">
     <button @click="setGoal">set goal</button>
   </div>
-  <error-alert v-if="isValueInvalid">
-    <h2>Input is invalid!</h2>
-    <p>Please enter atleast some characters...</p>
-    <button @click="confirmError">Okay</button>
-  </error-alert>
+  <!-- <teleport> tag will help us to render the code at desired area -->
+  <teleport to="body">
+    <error-alert v-if="isValueInvalid">
+      <h2>Input is invalid!</h2>
+      <p>Please enter atleast some characters...</p>
+      <button @click="confirmError">Okay</button>
+    </error-alert>
+  </teleport>
 </template>
 
 <script>

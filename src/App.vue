14c9f5cd4602d2ff98ea137ba84e2cf9e9310d3f
Anand raja <meetanandns@gmail.com>
2022-01-12 13:02:51 +0000

    dif. ways of comp registration

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <the-header></the-header>
+    <!-- <TheHeader/> -->
     <badge-list></badge-list>
     <user-info
       :full-name="activeUser.name"
@@ -11,7 +12,13 @@
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue';
+
 export default {
+  components: {
+    // 'the-header': TheHeader,
+    TheHeader: TheHeader
+  },
   data() {
     return {
       activeUser: {
